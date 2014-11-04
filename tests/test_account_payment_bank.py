55c8c0204adca85adfc3c614d76e5ca0cb92d148
Sergi Almacellas Abellana <sergi@koolpi.com>
2014-11-04 13:00:34 +0100

    Migrate to version 3.4

--- tests/test_account_payment_bank.py
+++ tests/test_account_payment_bank.py
@@ -2,21 +2,13 @@
 # This file is part of account_payment_bank module for Tryton.
 # The COPYRIGHT file at the top level of this repository contains
 # the full copyright notices and license terms.
-from trytond.tests.test_tryton import test_view, test_depends
-import os
-import sys
-import trytond.tests.test_tryton
 import unittest
-DIR = os.path.abspath(os.path.normpath(os.path.join(__file__,
-    '..', '..', '..', '..', '..', 'trytond')))
-if os.path.isdir(DIR):
-    sys.path.insert(0, os.path.dirname(DIR))
+import trytond.tests.test_tryton
+from trytond.tests.test_tryton import test_view, test_depends
 
 
 class AccountPaymentBankTestCase(unittest.TestCase):
-    '''
-    Test Account Payment Bank module.
-    '''
+    'Test Account Payment Bank module'
 
     def setUp(self):
         trytond.tests.test_tryton.install_module('account_payment_bank')
@@ -28,9 +20,7 @@ class AccountPaymentBankTestCase(unittest.TestCase):
         test_view('account_payment_bank')
 
     def test0006depends(self):
-        '''
-        Test depends.
-        '''
+        'Test depends'
         test_depends()
 
 
@@ -39,6 +29,3 @@ def suite():
     suite.addTests(unittest.TestLoader().loadTestsFromTestCase(
         AccountPaymentBankTestCase))
     return suite
-
-if __name__ == '__main__':
-    unittest.TextTestRunner(verbosity=2).run(suite())
