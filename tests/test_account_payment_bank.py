ece8ee2f0650fbdd10a7c1ded0bb467e366afc79
resteve <resteve@zikzakmedia.com>
2015-12-01 10:59:33 +0100

    Tests

--- tests/test_account_payment_bank.py
+++ tests/test_account_payment_bank.py
@@ -1,27 +1,14 @@
-#!/usr/bin/env python
-# This file is part of account_payment_bank module for Tryton.
-# The COPYRIGHT file at the top level of this repository contains
-# the full copyright notices and license terms.
+# This file is part of the account_payment_bank module for Tryton.
+# The COPYRIGHT file at the top level of this repository contains the full
+# copyright notices and license terms.
 import unittest
 import trytond.tests.test_tryton
-from trytond.tests.test_tryton import test_view, test_depends
+from trytond.tests.test_tryton import ModuleTestCase
 
 
-class AccountPaymentBankTestCase(unittest.TestCase):
+class AccountPaymentBankTestCase(ModuleTestCase):
     'Test Account Payment Bank module'
-
-    def setUp(self):
-        trytond.tests.test_tryton.install_module('account_payment_bank')
-
-    def test0005views(self):
-        '''
-        Test views.
-        '''
-        test_view('account_payment_bank')
-
-    def test0006depends(self):
-        'Test depends'
-        test_depends()
+    module = 'account_payment_bank'
 
 
 def suite():
